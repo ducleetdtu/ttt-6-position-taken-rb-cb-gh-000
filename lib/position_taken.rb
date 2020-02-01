09160ddf5f5a5417d745d9db1622ec3b5d1b32b7
Duc Le Dang <51703059@student.tdtu.edu.vn>
2020-02-01 05:44:01 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
-def position_taken?(arr,ind)
-  if arr[idx] == " " || arr[idx] == "" || arr[idx] == nil
+def position_taken?(board,index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
     return false
-  elsif arr[idx] == "X" || arr[idx] == "O"
+  elsif board[index] == "X" || board[index] == "O"
     return true
   end
 end
